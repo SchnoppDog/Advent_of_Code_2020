cfe894155b2fc2af31b74559532d0c492de4bed9
jm90bari <jannik.meyer@tu-darmstadt.de>
2020-12-10 15:46:52 +0100

    Day6 finish

--- src/day5.py
+++ src/day5.py
@@ -7,12 +7,11 @@ def task1():
 
     with open("files_for_days/day5.txt", "r") as file:
         for line in file:
-            lineLetter = line[7]
+            lineLetter = line[7]        # Is either R or L
             line = line.replace("\n", "")
-            line = line.split(line[7], 1)
-            line[1] = lineLetter + line[1]
-            lines.append(line)
-        print(lines)
+            line = line.split(line[7], 1)       # Splitting on position 7
+            line[1] = lineLetter + line[1]      # Adding Position 7 to the front since it has been split
+            lines.append(line)      # Returns: [["FBFBBFF", "RLR"], ["BFFFBBF", "RRR"], ...]
 
     for lineList in lines:
         upperHalfFB = 128
@@ -25,18 +24,24 @@ def task1():
         for fbLetter in lineList[0]:
             if fbLetter == "F":
                 upperHalfFB = ((upperHalfFB - lowerHalfFB) / 2) + lowerHalfFB
-                # upperHalfBinary = upperHalf - 1
+                # i.e: 1. 128 = ((128 - 0) / 2) + 0 = 64
+                # i.e: 3. 64 = ((64 - 32) / 2) + 32 = 48
+                # ...
 
             elif fbLetter == "B":
                 lowerHalfFB = ((upperHalfFB - lowerHalfFB) / 2) + lowerHalfFB
+                # i.e: 2. 0 = ((64 - 0) / 2) + 0 = 32
+                # i.e: 4. 32 = ((48 - 32) / 2) + 32 = 40
+                # ...
 
         if lineList[0][lenFbList] == "F":
-            upperHalfBinaryFB = upperHalfFB - 1
+            upperHalfBinaryFB = upperHalfFB - 1     # Binary Expression: 0-7 (8), 0-15 (16), 0-31 (32) etc.
             fbValues.append(upperHalfBinaryFB)
 
         elif lineList[0][lenFbList] == "B":
             fbValues.append(lowerHalfFB)
 
+        # Same procedure like F and B
         for rlLetter in lineList[1]:
             if rlLetter == "R":
                 lowerHalfRL = ((upperHalfRL - lowerHalfRL) / 2) + lowerHalfRL
--- src/day6.py
+++ src/day6.py
@@ -1,4 +1,4 @@
-def task1():
+def task1And2():
     groups          = []
     nestedGroups    = []
     lines           = []
@@ -8,31 +8,37 @@ def task1():
 
     with open("files_for_days/day6.txt", "r") as file:
         for line in file:
-            line = line.rstrip()
-            lines.append(line)
-    
+            line = line.rstrip()    # Stripping \r\n each line
+            lines.append(line)      # Return Array with all groups plus empty line. Example: ["abc", "", "a", "b", "c", "", ...]
+
     for string in lines:
+        # If line is empty, add the nested group
         if string == "":
-            groups.append(nestedGroups)
+            groups.append(nestedGroups)     # Returns: [["abc"], ["a", "b", "c"], ...]
             nestedGroups = []
             countaddGroups += 1
             continue
-        
+        # Creating each group unitl empty line
         else:
             nestedGroups.append(string)
+            # Appending the last group
             if countaddGroups == len(lines) - 1:
                 groups.append(nestedGroups)
-        
+
         countaddGroups += 1
 
+    # Tasks for every group
     for group in groups:
         # Task 1:
         groupAnswers    = group[0]
         # Task 2:
-        groupAnswersLen = len(group[0])
+        groupLen        = len(group)
+        letterDict      = {}
+        answerString    = ""
+
         if len(group) == 1:
-            # Task 1:
-            groupYesCounts.append(len(group[0]))
+            # Task 1 and 2:
+            groupYesCounts.append(len(group[0]))    # If a group has only 1 member, each answer is accepted as "yes"
             continue
 
         for answer in group:
@@ -41,23 +47,25 @@ def task1():
                 # if letter not in groupAnswers:
                 #     groupAnswers = groupAnswers + letter
                 # Task 2:
-                if letter not in groupAnswers:
-                    groupAnswersLen -= 1
+                if letter in letterDict.keys():
+                    letterDict[letter] = letterDict[letter] + 1         # E.g: letterDict = { "a": 1, "b": 1, "c": 1} -> "a" in dict = letterDict = { 
+                                                                        # "a": 2, "b": 1, "c": 1}
+                else:
+                    letterDict[letter] = 1
 
         # Task 1:
         # groupYesCounts.append(len(groupAnswers))
         # Task 2:
-        if groupAnswersLen <= 0:
-            groupAnswersLen = 0
+        for letter in letterDict.keys():
+            if letterDict[letter] == groupLen:          # All correct letters have to be the equal to the length of the group (members)
+                answerString = answerString + letter
 
-            # Idea Task2: -> Match the answers after the first one with the first one and make a new String which contains all letter everyone crossed with a yes
+        groupYesCounts.append(len(answerString))
 
-        groupYesCounts.append(groupAnswersLen)
-    
     for yesCounts in groupYesCounts:
         yesSum += yesCounts
-    
-    print("Sum of Yes-Answers: ", yesSum)
-    
 
-task1()
+    print("Sum of Yes-Answers: ", yesSum)
+
+
+task1And2()
